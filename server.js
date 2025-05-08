f4d64369e391778458d8bdec79a9a39d02a65969
masternpolo <masternpolo@gmail.com>
2025-05-08 15:31:39 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const port = 3300;
+const https = require('https')
+
+
+app.get('/paystack', (req, res) => {
+    const https = require('https')
+
+    const params = JSON.stringify({
+        "email": "customer@email.com",
+        "amount": "500000"
+    })
+
+    const options = {
+        hostname: 'api.paystack.co',
+        port: 443,
+        path: '/transaction/initialize',
+        method: 'POST',
+        headers: {
+            Authorization: 'Bearer sk_test_1376138953d227c699664afe2951392ff2f9bfd2',
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const reqPastack = https.request(options, resPaystack => {
+        let data = ''
+
+        res.on('data', (chunk) => {
+            data += chunk
+        });
+
+        res.on('end', () => {
+            console.log(JSON.parse(data))
+        })
+    }).on('error', error => {
+        console.error(error)
+    })
+
+    reqPastack.write(params)
+    reqPastack.end()
+})
+
+app.listen(port, () => {
+    console.log('app is listening on port 3300');
+})
