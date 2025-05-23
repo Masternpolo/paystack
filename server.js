888540d3ec12f0f77f8bf3e5c42883d6cc76f386
masternpolo <masternpolo@gmail.com>
2025-05-23 14:52:19 +0100

    checking transaction status 4

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = 3300;
 const https = require('https')
 const cors = require('cors')
 const pool = require('./database/db.js')
+require('dotenv').config({
+    path: './config.env'
+})
 
 app.use(cors())
 
@@ -12,70 +15,67 @@ let reference;
 let trxref;
 
 const crypto = require('crypto');
-const secret = 'sk_test_1376138953d227c699664afe2951392ff2f9bfd2';
+const path = require('path');
+const secret = '';
 // Using Express
-app.post("/paystack/webhook", async function(req, res) {
+app.post("/paystack/webhook", async function (req, res) {
     //validate event
     const hash = crypto.createHmac('sha512', secret).update(JSON.stringify(req.body)).digest('hex');
     if (hash == req.headers['x-paystack-signature']) {
-    // Retrieve the request's body
-    const event = req.body;
-    // Do something with event  
-   const sql = 'INSERT INTO data (data) VALUES ($1)';
-    const values = [JSON.stringify(event)];
-    const result = await pool.query(sql, values)
+        // Retrieve the request's body
+        const event = req.body;
+        // Do something with event  
+        const sql = 'INSERT INTO data (data) VALUES ($1)';
+        const values = [JSON.stringify(event)];
+        const result = await pool.query(sql, values)
     }
     res.send(200);
 });
 
 app.get('/callback', (req, res, next) => {
-    reference  = req.query.reference;
+    reference = req.query.reference;
     trxref = req.query.trxref;
     console.log(reference);
     res.send(`callback url route activated , ${reference} : trxref : ${trxref}`)
-   
+
 })
 
 app.get('/verify', (req, res, next) => {
-    const https = require('https')
 
-const options = {
-  hostname: 'api.paystack.co',
-  port: 443,
-  path: `/transaction/verify/:${reference}`,
-  method: 'GET',
-  headers: {
-    Authorization: 'Bearer sk_test_1376138953d227c699664afe2951392ff2f9bfd2'
-  }
-}
+    const options = {
+        hostname: 'api.paystack.co',
+        port: 443,
+        path: `/transaction/verify/:${reference}`,
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+            'Content-Type': 'application/json'
+        }
+    }
 
-https.request(options, res => {
-  let data = ''
+    https.request(options, res => {
+        let data = ''
 
-  res.on('data', (chunk) => {
-    data += chunk
-  });
+        res.on('data', (chunk) => {
+            data += chunk
+        });
 
-  res.on('end', () => {
-    console.log(JSON.parse(data))
+        res.on('end', () => {
+            console.log(JSON.parse(data))
+        })
+    }).on('error', error => {
+        console.error(error)
+    })
     res.send(data.status)
-  })
-}).on('error', error => {
-  console.error(error)
-})
+
 })
 
 
 app.get('/paystack', (req, res) => {
-    const https = require('https');
+
     const email = 'masternpolo@gmail.com'
-    const amount = 50 * 100;
+    const amount = 5000 * 100;
     const callback_url = 'https://paystack-ri87.onrender.com/callback'
-    const failure_url = 'http://localhost:3300/failure'
-
-
-    console.log(email, amount);
-    
 
     const params = JSON.stringify({
         email,
@@ -89,7 +89,7 @@ app.get('/paystack', (req, res) => {
         path: '/transaction/initialize',
         method: 'POST',
         headers: {
-            Authorization: 'Bearer sk_test_1376138953d227c699664afe2951392ff2f9bfd2',
+            Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
             'Content-Type': 'application/json'
         }
     }
